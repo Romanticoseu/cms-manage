cba56218db4c268440d41386ce138868594b5d8f
Romanticoseu <1251448395@qq.com>
2022-04-27 19:33:29 +0800

    Route

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './assets/base.css'
+import { Button } from 'antd';
+import { Outlet } from "react-router-dom";
+
+export default function App() {
+    return(
+        <>
+            <Button type="primary">Primary Button</Button>
+
+            <div>App</div>
+            <Outlet />
+        </>
+        
+    )
+}
